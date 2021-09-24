2b80bf0d1fcf6bdf224279a9381e4b779bbd771f
xulihang <xulihanghai@163.com>
2021-09-24 13:38:25 +0800

    add elapsed time to reader

--- aggregated_reader.py
+++ aggregated_reader.py
@@ -1,3 +1,5 @@
+import time
+
 class AggregatedReader():
 
     def __init__(self, engine="dynamsoft"):
@@ -46,5 +48,10 @@ class AggregatedReader():
                 self.reader.set_runtime_settings_with_template(settings)
             except:
                 print("wrong settings")
-        return self.reader.decode_file(file_path)
+        start_time = time.time()
+        results = self.reader.decode_file(file_path)
+        end_time = time.time()
+        elapsedTime = int((end_time - start_time) * 1000)
+        results["elapsedTime"] = elapsedTime
+        return results
         
