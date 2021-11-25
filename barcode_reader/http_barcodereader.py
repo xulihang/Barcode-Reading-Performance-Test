560ed92bb9ab8f88f4632ab8a05a113d3af17d07
xulihang <xulihanghai@163.com>
2021-11-25 17:43:58 +0800

    fix url

--- barcode_reader/http_barcodereader.py
+++ barcode_reader/http_barcodereader.py
@@ -5,7 +5,7 @@ import base64
 
 
 class HTTPBarcodeReader():
-    def __init__(self, sdk="MLKit",url="http://192.168.31.103:8888/"):
+    def __init__(self, sdk="MLKit",url="http://192.168.8.68:8888/"):
         self.url = url
         self.sdk = sdk
         if os.path.exists("qrcode.png"):
@@ -14,9 +14,8 @@ class HTTPBarcodeReader():
     def decode_file(self, img_path):
         with open(img_path, "rb") as img_file:
             b64_string = base64.b64encode(img_file.read())
-        print(b64_string)
-        r = requests.post('http://192.168.31.103:8888/', json = {'base64':b64_string.decode("utf-8"),'sdk':self.sdk})
-        result_dict = r.text
+        r = requests.post(self.url, json = {'base64':b64_string.decode("utf-8"),'sdk':self.sdk})
+        result_dict = json.loads(r.text)
         return result_dict
         
 if __name__ == '__main__':
